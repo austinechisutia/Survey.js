40a64e7a9fa478333ea8d7b39ddd9ce9285b70c1
AUstine <austineakhonya624@gmail.com>
2025-12-03 09:08:03 +0300

    add

--- app/components/Survey.js
+++ app/components/Survey.js
@@ -7,61 +7,209 @@ import "../surveyStyles.css";
 
 
 const surveyJson = {
-   title: "Better Evidence",
-
+  title: "Better Evidence",
+  checkErrorsMode: "onValueChanged",
+  textUpdateMode: "onTyping",
   showProgressBar: "top",
-  pages: [{
-    name: "PersonalDetails",
-    title: "Personal Info",
-    elements: [{
-      type: "text",
-      name: "First Name",
-      title: "First Name",
-      placeholder: "Abebe"
+  pages: [
+    {
+      name: "page1",
+      title: "Personal Info",
+      elements: [
+        {
+          type: "text",
+          name: "First Name",
+          title: "First Name",
+          placeholder: "Abebe",
+          isRequired: true,
+          requiredErrorText: "Value cannot be empty",
+        },
+        {
+          type: "text",
+          name: "Last Name",
+          title: "Last Name",
+          placeholder: "Kebende",
+          isRequired: true,
+          requiredErrorText: "Value cannot be empty",
+        },
+        {
+          type: "dropdown",
+          name: "Title/Degree",
+          title: "Title/Degree",
+          choices: ["Mr.", "Mrs.", "Dr.", "Prof"],
+          isRequired: true,
+          requiredErrorText: "Value cannot be empty",
+        },
+        {
+          type: "rating",
+          name: "Gender",
+          title: "Gender",
+          width: "100%",
+          rateCount: 3,
+          rateValues: [
+            {
+              value: 1,
+              text: "Male",
+            },
+            {
+              value: 2,
+              text: "Women",
+            },
+            {
+              value: 3,
+              text: "Prefer not to say",
+            },
+          ],
 
+          isRequired: true,
+          requiredErrorText: "Value cannot be empty",
+        },
+        {
+          type: "text",
+          inputType: "number",
+          name: "Age",
+          title: "Age",
+          placeholder: "Enter your Age",
+          isRequired: true,
+          requiredErrorText: "Value cannot be empty",
+        },
+        {
+          type: "text",
+          inputType: "email",
+          name: "Email",
+          title: "Email",
+          placeholder: "user@gmail.com",
+          isRequired: true,
+          requiredErrorText: "Value cannot be empty",
+        },
+        {
+          type: "text",
+          inputType: "number",
+          name: "Age",
+          title: "Phone Number",
+          placeholder: "+254768400317",
+          isRequired: true,
+          requiredErrorText: "Value cannot be empty",
+        },
+        {
+          type: "boolean",
+          name: "enableFeature",
+          title: "Recieve Updates on Whatsapp",
+          titleLocation: "left",
+          startWithNewLine: true,
+          cssClasses: {
+            root: "toggle-row",
+          },
+        },
+        {
+          type: "text",
+          inputType: "number",
+          name: "Age",
+          title: "Whatsapp Number",
+          placeholder: "+254768400317",
+          isRequired: true,
+          requiredErrorText: "Value cannot be empty",
+        },
+      ],
+    },
+
+    {
+      name: "page2",
+      title: "Where do you Practice",
+      elements: [
+        {
+          type: "dropdown",
+          name: "country",
+          title: "Country",
+          choicesByUrl: {
+            url: "https://surveyjs.io/api/CountriesExample",
+            valueName: "name",
+          },
+          isRequired: true,
+          requiredErrorText: "Value cannot be empty",
+        },
+        {
+          type: "text",
+          name: "Facility Name",
+          title: "Facility Name",
+          isRequired: true,
+          requiredErrorText: "Value cannot be empty",
+        },
+        {
+          type: "panel",
+          name: "Details",
+          title: "Practice Details",
+          elements: [
+            {
+              type: "dropdown",
+              name: "Role/Profession",
+              title: "Role/Profession",
+            },
+            {
+              type: "dropdown",
+              name: "Speciality",
+              title: "Primary Speciality",
+            },
+            {
+              type: "dropdown",
+              name: "Practice",
+              title: "Practice Setting",
+            },
+          ],
+        },
+        {
+          type: "boolean",
+          name: "Patient",
+          title: "Patient Care",
+        },
+        {
+          type: "boolean",
+          name: "Payment",
+          title: "Payment",
+        },
+        {
+          type: "text",
+          inputType: "number",
+          name: "Payment",
+          title: "Average Patients Per week",
+        },
+      ],
     },
     {
-      type: "text",
-      name: "Last Name",
-      title: "Last Name",
-      placeholder: "Kebende"
-    },
-     {
-      type: "dropdown",
-      name: "Title/Degree",
-      title: "Title/Degree",
-      choices:[
-      "Mr.",
-        "Mrs.",
-        "Dr.",
-        "Prof"
-     ]
-      
-    }
-  ]
-  },{
-    title: "Where do you Practice",
+      name: "page3",
+      title: "Verify and Submit",
+      width: "600%",
 
-     elements: [{
-            name: "how-can-we-improve",
-            title: "Country?",
-            type: "dropdown",
-            enableSearch: true,
-            choicesByUrl: {
-            url: "https://surveyjs.io/api/CountriesExample",
-            valueName: "name", // use country name
-  }
+      elements: [
+        {
+          type: "text",
+          name: "full-name",
+          title: "Identity Check(Veriff)",
+          description: "Pending...",
         },
-       {
-    name: "Speciality",
-    title: "Primary Speciality",
-    type: "dropdowm"
-  }],
-  },
-],
-  
-  
-}
+        {
+          type: "image",
+          name: "textImageSection",
+          imageLink:
+            "https://www.pexels.com/photo/cityscape-of-naples-with-view-of-the-galleria-umberto-i-17567462/",
+          startWithNewLine: false,
+        },
+        {
+          type: "text",
+          name: "secure",
+          title: "Verification",
+          placeholder: "Start secure identity check",
+          description: "We use verif to securely confirm your id. It takes 2 minutes"
+        },
+        
+      ],
+    },
+  ],
+  questionErrorLocation: "bottom",
+  pagePrevText: "Back",
+  pageNextText: "Next Step",
+  completeText: "Submit Apllication",
+};
 
 
 
